3e5ceaa340c8111b2171e1d126760400974bdc21
jcsl-ca <jcsanchezlozano@gmail.com>
2025-03-19 21:34:07 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)  # Initialize Flask app
 
+# New line
 @app.route("/")  # Define a route for the homepage
 def home():
     return "<h1>Welcome to My Flask App!</h1><p>This is a simple Flask application.</p>"
